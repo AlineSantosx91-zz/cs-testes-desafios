ba4d4545fd78c42b60e7de30e76ce5f91e89f104
Aline Santos <aline.r.santos@accenture.com>
2020-10-02 15:59:13 -0300

    refactore packages

---
+++ src/main/java/br/com/concrete/desafios/routemamager/repository/RouteManagerRepository.java
@@ -0,0 +1,8 @@
+package br.com.concrete.desafios.routemamager.repository;
+
+import java.util.List;
+
+public interface RouteManagerRepository {
+    public List<String> findAllRoutes();
+
+}
---
+++ src/main/java/br/com/concrete/desafios/routemamager/service/RouteManagerService.java
@@ -0,0 +1,33 @@
+package br.com.concrete.desafios.routemamager.service;
+
+import br.com.concrete.desafios.routemamager.repository.RouteManagerRepository;
+import java.util.List;
+
+public class RouteManagerService {
+
+    private final RouteManagerRepository routeManagerRepository;
+
+    public RouteManagerService(RouteManagerRepository routeManagerRepository) {
+        this.routeManagerRepository = routeManagerRepository;
+    }
+
+    public Boolean checkRoutes(final String routeParam) {
+        final List<String> routes = routeManagerRepository.findAllRoutes();
+
+        for (String route : routes) {
+
+            if (!route.contains("user_id"))  return routes.contains(routeParam);
+
+            if (route.split("/")[2].contains("user_id")){
+
+                String[] routeSplit = routeParam.split("/");
+                if ( routeSplit.length >= 3 && route.contains(routeSplit[1])) {
+                    if (routeParam.charAt(0) == '/') {
+                        return "/".concat(route.split("/")[1]).concat("/".concat(routeSplit[2])).equals(routeParam);
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
